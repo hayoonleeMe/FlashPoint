4cf190c90643ad5cf7052c8ba06835f8778c5c94
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-05-30 22:45:57 +0900

    UFPAssetData에 설정된 애셋을 동기, 비동기 방식으로 로드하는 기능을 제공하는 Custom Asset Manager class UFPAssetManager 생성 - Custom Log Category 추가 - UFPAssetManager 클래스를 사용하도록 프로젝트 설정

---
+++ Source/FlashPoint/Data/FPAssetData.h
@@ -0,0 +1,54 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "FPAssetData.generated.h"
+
+USTRUCT()
+struct FAssetEntry
+{
+	GENERATED_BODY()
+	
+	FAssetEntry()
+		: bPreLoad(false)
+	{ }
+
+	UPROPERTY(EditDefaultsOnly)
+	FSoftObjectPath AssetPath;
+
+	// true면, 이 애셋은 UFPAssetManager에 의해 게임 시작 시 로드된다.
+	UPROPERTY(EditDefaultsOnly)
+	uint8 bPreLoad : 1;
+};
+
+/**
+ * FPAssetManager로 로드할 애셋의 정보를 저장하는 Primary Data Asset
+ */
+UCLASS(Const)
+class FLASHPOINT_API UFPAssetData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	// 애셋을 저장하기 전에 호출되어 AssetIdToPath를 채운다.
+	virtual void PreSave(FObjectPreSaveContext SaveContext) override;
+
+#if WITH_EDITOR
+	virtual EDataValidationResult IsDataValid(FDataValidationContext& Context) const override;
+#endif
+
+	// AssetId에 대한 AssetPath를 반환한다.
+	FSoftObjectPath GetAssetPathById(const FName& AssetId) const;
+
+	// 모든 Preload할 애셋의 AssetPath TArray를 반환한다.
+	TArray<FSoftObjectPath> GetPreloadAssetPaths() const;
+
+private:
+	UPROPERTY(EditDefaultsOnly)
+	TMap<FName, FAssetEntry> AssetIdToEntry;
+
+	UPROPERTY()
+	TMap<FName, FSoftObjectPath> AssetIdToPath;
+};
---
+++ Source/FlashPoint/System/FPAssetManager.cpp
@@ -0,0 +1,163 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPAssetManager.h"
+
+#include "FPLogChannels.h"
+#include "Data/FPAssetData.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FPAssetManager)
+
+UFPAssetManager& UFPAssetManager::Get()
+{
+	if (UFPAssetManager* AssetManager = Cast<UFPAssetManager>(GEngine->AssetManager))
+	{
+		return *AssetManager;
+	}
+
+	UE_LOG(LogFP, Fatal, TEXT("Can't find UFPAssetManager. AssetManagerClassName in DefaultEngine.ini must be set to FPAssetManager."));
+
+	// Fatal Log로 인해 실제로 수행되지 않음
+	return *NewObject<UFPAssetManager>();
+}
+
+void UFPAssetManager::Initialize()
+{
+	Get().LoadPreloadAssets();
+}
+
+UObject* UFPAssetManager::LoadSyncByPath(const FSoftObjectPath& AssetPath, bool bKeepInMemory)
+{
+	if (AssetPath.IsValid())
+	{
+		UObject* LoadedAsset = AssetPath.ResolveObject();
+		if (!LoadedAsset)
+		{
+			if (IsInitialized())
+			{
+				LoadedAsset = GetStreamableManager().LoadSynchronous(AssetPath);
+			}
+			else
+			{
+				LoadedAsset = AssetPath.TryLoad();
+			}
+		}
+
+		if (LoadedAsset)
+		{
+			if (bKeepInMemory)
+			{
+				Get().AddLoadedAsset(LoadedAsset);
+			}
+		}
+		else
+		{
+			UE_LOG(LogFP, Fatal, TEXT("Failed to load asset %s synchronously."), *AssetPath.ToString());
+		}
+
+		return LoadedAsset;
+	}
+
+	return nullptr;
+}
+
+UObject* UFPAssetManager::LoadSyncById(const FName& AssetId, bool bKeepInMemory)
+{
+	const UFPAssetData* AssetData = Get().LoadedAssetData;
+	check(AssetData);
+	
+	const FSoftObjectPath& AssetPath = AssetData->GetAssetPathById(AssetId);
+	return LoadSyncByPath(AssetPath, bKeepInMemory);
+}
+
+void UFPAssetManager::LoadAsyncByPath(const FSoftObjectPath& AssetPath, FAsyncLoadCompletedDelegate AsyncLoadCompletedDelegate, bool bKeepInMemory)
+{
+	if (!IsInitialized())
+	{
+		AsyncLoadCompletedDelegate.ExecuteIfBound(nullptr);
+		UE_LOG(LogFP, Error, TEXT("AssetManager must be initialized for async loading."));
+		return;
+	}
+
+	if (!AssetPath.IsValid())
+	{
+		AsyncLoadCompletedDelegate.ExecuteIfBound(nullptr);
+		UE_LOG(LogFP, Error, TEXT("Invalid Asset Path %s"), *AssetPath.ToString());
+		return;
+	}
+
+	const TSharedPtr<FStreamableHandle> Handle = GetStreamableManager().RequestAsyncLoad(AssetPath);
+	Handle->BindCompleteDelegate(FStreamableDelegate::CreateLambda([AssetPath, bKeepInMemory, CompletedDelegate = MoveTemp(AsyncLoadCompletedDelegate)]()
+	{
+		if (UObject* LoadedAsset = AssetPath.ResolveObject())
+		{
+			if (bKeepInMemory)
+			{
+				Get().AddLoadedAsset(LoadedAsset);
+			}
+					
+			CompletedDelegate.ExecuteIfBound(LoadedAsset);
+		}
+	}));
+}
+
+void UFPAssetManager::LoadAsyncById(const FName& AssetId, FAsyncLoadCompletedDelegate AsyncLoadCompletedDelegate, bool bKeepInMemory)
+{
+	const UFPAssetData* AssetData = Get().LoadedAssetData;
+	check(AssetData);
+	
+	const FSoftObjectPath& AssetPath = AssetData->GetAssetPathById(AssetId);
+	LoadAsyncByPath(AssetPath, MoveTemp(AsyncLoadCompletedDelegate), bKeepInMemory);
+}
+
+void UFPAssetManager::ReleaseAll()
+{
+	UFPAssetManager& AssetManager = Get();
+	FScopeLock ScopeLock(&AssetManager.LoadedAssetsCritical);
+	AssetManager.LoadedAssets.Reset();
+}
+
+void UFPAssetManager::LoadPreloadAssets()
+{
+	if (LoadedAssetData)
+	{
+		return;
+	}
+
+	UFPAssetData* AssetData = nullptr;
+	
+	// DefaultGame.ini - AssetManagerSettings - PrimaryAssetTypesToScan에 설정된 Type의 애셋을 로드한다.
+	const FPrimaryAssetType PrimaryAssetType(UFPAssetData::StaticClass()->GetFName());
+	const TSharedPtr<FStreamableHandle> Handle = LoadPrimaryAssetsWithType(PrimaryAssetType);
+	
+	if (Handle.IsValid())
+	{
+		Handle->WaitUntilComplete(0.f, false);
+		AssetData = Cast<UFPAssetData>(Handle->GetLoadedAsset());
+	}
+	
+	if (AssetData)
+	{
+		LoadedAssetData = AssetData;
+
+		// Preload를 수행해야 할 내부 애셋을 모두 로드한다.
+		TArray<FSoftObjectPath> PreloadAssetPaths = LoadedAssetData->GetPreloadAssetPaths();
+		for (const FSoftObjectPath& Path : PreloadAssetPaths)
+		{
+			LoadSyncByPath(Path);
+		}
+	}
+	else
+	{
+		UE_LOG(LogFP, Fatal, TEXT("Failed to load PrimaryAssetType %s"), *PrimaryAssetType.ToString());
+	}
+}
+
+void UFPAssetManager::AddLoadedAsset(const UObject* LoadedAsset)
+{
+	if (LoadedAsset)
+	{
+		FScopeLock ScopeLock(&LoadedAssetsCritical);
+		LoadedAssets.Add(LoadedAsset);
+	}
+}
