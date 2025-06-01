0b7ceba77e4b4b8183847ba9defa2151ca8e200e
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-01 23:15:38 +0900

    native input system (#9)
    * UFPInputData Custom DataAsset 클래스 생성 및 블루프린트 생성
    - Default Input Mapping Context 애셋 생성 및 설정
    
    * UFPInputData의 정보를 가지고 일반 입력을 바인딩하는 InputComponent 클래스 생성
    - EnhancedInput Module 추가
    - 기본 InputComponent class로 FPInputComponent 사용하도록 설정
    
    * DA_AssetData에 InputData - DA_InputData 데이터 추가
    
    * Custom PlayerController 클래스 생성
    FPPlayerController에서 기본 InputMappingContext를 생성하고, 일반 입력을 연동하도록 구현

---
+++ Source/FlashPoint/Input/FPInputComponent.h
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EnhancedInputComponent.h"
+#include "GameplayTagContainer.h"
+#include "Data/FPInputData.h"
+#include "FPInputComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FLASHPOINT_API UFPInputComponent : public UEnhancedInputComponent
+{
+	GENERATED_BODY()
+
+public:
+	// InputTag에 해당하는 Native InputAction의 TriggerEvent에 Func를 등록한다.
+	template<class UserClass, typename FuncType>
+	void BindNativeAction(const UFPInputData* InputData, const FGameplayTag& InputTag, ETriggerEvent TriggerEvent, UserClass* Object, FuncType Func);
+};
+
+template <class UserClass, typename FuncType>
+void UFPInputComponent::BindNativeAction(const UFPInputData* InputData, const FGameplayTag& InputTag, ETriggerEvent TriggerEvent, UserClass* Object, FuncType Func)
+{
+	check(InputData);
+
+	if (const UInputAction* InputAction = InputData->FindNativeInputActionForInputTag(InputTag))
+	{
+		BindAction(InputAction, TriggerEvent, Object, Func);
+	}
+}
