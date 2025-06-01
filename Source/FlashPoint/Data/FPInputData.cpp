1115629e44ddd344e04fcb522e29a0bc8d7f444b
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-01 23:03:40 +0900

    UFPInputData Custom DataAsset 클래스 생성 및 블루프린트 생성 - Default Input Mapping Context 애셋 생성 및 설정

---
+++ Source/FlashPoint/Data/FPInputData.cpp
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPInputData.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FPInputData)
+
+const UInputAction* UFPInputData::FindNativeInputActionForInputTag(const FGameplayTag& InputTag) const
+{
+	for (const FInputConfig& InputConfig : NativeInputConfigs)
+	{
+		if (InputConfig.InputTag.MatchesTag(InputTag))
+		{
+			return InputConfig.InputAction;
+		}
+	}
+	return nullptr;
+}
---
+++ Source/FlashPoint/Data/FPInputData.h
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "FPInputData.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+USTRUCT()
+struct FInputConfig
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UInputAction> InputAction;
+
+	UPROPERTY(EditDefaultsOnly)
+	FGameplayTag InputTag;
+};
+
+/**
+ * 
+ */
+UCLASS()
+class FLASHPOINT_API UFPInputData : public UDataAsset
+{
+	GENERATED_BODY()
+
+public:
+	// NativeInputConfigs에서 InputTag에 해당하는 InputAction을 찾아 반환한다.
+	// 찾지 못하면 nullptr를 반환한다.
+	const UInputAction* FindNativeInputActionForInputTag(const FGameplayTag& InputTag) const;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UInputMappingContext> DefaultMappingContext;
+
+	// 일반적인 입력
+	UPROPERTY(EditDefaultsOnly)
+	TArray<FInputConfig> NativeInputConfigs;
+};
