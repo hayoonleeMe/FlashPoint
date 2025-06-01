67bfc550a118dbe0bbb3a1ba87d48150773cf14b
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-01 23:14:47 +0900

    Custom PlayerController 클래스 생성 FPPlayerController에서 기본 InputMappingContext를 생성하고, 일반 입력을 연동하도록 구현

---
+++ Source/FlashPoint/Player/FPPlayerController.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "Data/FPInputData.h"
+#include "Input/FPInputComponent.h"
+#include "System/FPAssetManager.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FPPlayerController)
+
+AFPPlayerController::AFPPlayerController()
+{
+	bReplicates = true;
+}
+
+void AFPPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	const UFPInputData* InputData = UFPAssetManager::GetAssetById<UFPInputData>(TEXT("InputData"));
+	check(InputData);
+	
+	if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(InputData->DefaultMappingContext, 0);
+	}
+}
+
+void AFPPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	const UFPInputData* InputData = UFPAssetManager::GetAssetById<UFPInputData>(TEXT("InputData"));
+	check(InputData);
+
+	UFPInputComponent* FPInputComponent = CastChecked<UFPInputComponent>(InputComponent);
+
+	// Bind Native Inputs
+}
---
+++ Source/FlashPoint/Player/FPPlayerController.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "FPPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLASHPOINT_API AFPPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	AFPPlayerController();
+	
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+};
