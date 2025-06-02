3c24a07b21aa2781caf9645f37d0aa8ece9bfc20
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-02 17:54:24 +0900

    플레이어의 ASC, AS를 생성하고 저장할 PlayerState class 생성

---
+++ Source/FlashPoint/Player/FPPlayerState.cpp
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPPlayerState.h"
+
+#include "AbilitySystem/FPAbilitySystemComponent.h"
+#include "AbilitySystem/Attributes/FPAttributeSet.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FPPlayerState)
+
+AFPPlayerState::AFPPlayerState()
+{
+	NetUpdateFrequency = 120.f;
+	MinNetUpdateFrequency = 60.f;
+	
+	AbilitySystemComponent = CreateDefaultSubobject<UFPAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+	
+	AttributeSet = CreateDefaultSubobject<UFPAttributeSet>(TEXT("AttributeSet"));
+}
+
+UAbilitySystemComponent* AFPPlayerState::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+UFPAbilitySystemComponent* AFPPlayerState::GetFPAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+UFPAttributeSet* AFPPlayerState::GetFPAttributeSet() const
+{
+	return AttributeSet;
+}
---
+++ Source/FlashPoint/Player/FPPlayerState.h
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemInterface.h"
+#include "GameFramework/PlayerState.h"
+#include "FPPlayerState.generated.h"
+
+class UFPAttributeSet;
+class UFPAbilitySystemComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class FLASHPOINT_API AFPPlayerState : public APlayerState, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	AFPPlayerState();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+	UFPAbilitySystemComponent* GetFPAbilitySystemComponent() const;
+	UFPAttributeSet* GetFPAttributeSet() const;
+
+protected:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UFPAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY()
+	TObjectPtr<UFPAttributeSet> AttributeSet;
+};
