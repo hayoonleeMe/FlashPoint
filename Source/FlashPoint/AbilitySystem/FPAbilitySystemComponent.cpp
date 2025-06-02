8cf3c821f83366f249c22c36a15ea982febad316
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-02 17:53:25 +0900

    Custom AbilitySystemComponent class UFPAbilitySystemComponent 생성 Custom AttributeSet class UFPAttributeSet 생성 GAS 관련 Module 추가

---
+++ Source/FlashPoint/AbilitySystem/FPAbilitySystemComponent.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPAbilitySystemComponent.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FPAbilitySystemComponent)
+
+UFPAbilitySystemComponent::UFPAbilitySystemComponent()
+{
+}
+
+void UFPAbilitySystemComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
