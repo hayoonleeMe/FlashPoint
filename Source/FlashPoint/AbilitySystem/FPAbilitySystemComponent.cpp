385ae60e3fcf0bbd2aaf534940f90d375ec2bf41
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-03 12:28:04 +0900

    어빌리티 입력이 발생하면 해당 어빌리티를 저장하도록 구현

--- Source/FlashPoint/AbilitySystem/FPAbilitySystemComponent.cpp
+++ Source/FlashPoint/AbilitySystem/FPAbilitySystemComponent.cpp
@@ -5,11 +5,41 @@
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(FPAbilitySystemComponent)
 
-UFPAbilitySystemComponent::UFPAbilitySystemComponent()
-{
-}
-
 void UFPAbilitySystemComponent::BeginPlay()
 {
 	Super::BeginPlay();
+}
+
+{
+
+void UFPAbilitySystemComponent::AbilityInputTagPressed(const FGameplayTag& InputTag)
+{
+	if (InputTag.IsValid())
+	{
+		for (const FGameplayAbilitySpec& Spec : ActivatableAbilities.Items)
+		{
+			if (Spec.DynamicAbilityTags.HasTagExact(InputTag))
+			{
+				InputPressedSpecHandles.AddUnique(Spec.Handle);
+				InputHeldSpecHandles.AddUnique(Spec.Handle);
+			}
+		}
+	}
+}
+
+void UFPAbilitySystemComponent::AbilityInputTagReleased(const FGameplayTag& InputTag)
+{
+	if (InputTag.IsValid())
+	{
+		for (const FGameplayAbilitySpec& Spec : ActivatableAbilities.Items)
+		{
+			if (Spec.DynamicAbilityTags.HasTagExact(InputTag))
+			{
+				// 해당 어빌리티
+				InputReleasedSpecHandles.AddUnique(Spec.Handle);
+				InputHeldSpecHandles.Remove(Spec.Handle);
+			}
+		}
+	}
+}
 }
--- Source/FlashPoint/AbilitySystem/FPAbilitySystemComponent.h
+++ Source/FlashPoint/AbilitySystem/FPAbilitySystemComponent.h
@@ -12,9 +12,27 @@ class FLASHPOINT_API UFPAbilitySystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
 
-public:
-	UFPAbilitySystemComponent();
-
 protected:
 	virtual void BeginPlay() override;
+
+public:
+	/// InputTag에 해당하는 입력이 Press되면 호출된다.
+	/// 이번 프레임에서 Input Press된 Ability Spec Handle을 저장하고, ProcessAbilityInput()에서 처리한다.
+	/// @see AFPPlayerController::Input_AbilityInputTagPressed
+	void AbilityInputTagPressed(const FGameplayTag& InputTag);
+
+	/// InputTag에 해당하는 입력이 Release되면 호출된다.
+	/// 이번 프레임에서 Input Released된 Ability Spec Handle을 저장하고, ProcessAbilityInput()에서 처리한다.
+	/// @see AFPPlayerController::Input_AbilityInputTagReleased
+	void AbilityInputTagReleased(const FGameplayTag& InputTag);
+	
+private:
+	// 이번 프레임에 Input Press된 Ability Spec Handle
+	TArray<FGameplayAbilitySpecHandle> InputPressedSpecHandles;
+
+	// 입력 유지 중인 Ability Spec Handle
+	TArray<FGameplayAbilitySpecHandle> InputHeldSpecHandles;
+
+	// 이번 프레임에 Input Release된 Ability Spec Handle
+	TArray<FGameplayAbilitySpecHandle> InputReleasedSpecHandles;
 };
