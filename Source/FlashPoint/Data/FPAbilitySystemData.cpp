3fed50b8acba326d280a9767d5eb51a1f1b90b4a
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-03 16:15:55 +0900

    부여할 Ability와 적용할 Effect를 저장하는 DataAsset class UFPAbilitySystemData 생성 -  부여된 Ability Handle, 적용된 Effect Handle을 저장하는 FFPAbilitySystemData_GrantedHandles 구조체 생성

---
+++ Source/FlashPoint/Data/FPAbilitySystemData.cpp
@@ -0,0 +1,50 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPAbilitySystemData.h"
+
+#include "AbilitySystem/FPAbilitySystemComponent.h"
+#include "AbilitySystem/Abilities/FPGameplayAbility.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(FPAbilitySystemData)
+
+void UFPAbilitySystemData::GiveDataToAbilitySystem(UFPAbilitySystemComponent* ASC, FFPAbilitySystemData_GrantedHandles* OutGrantedHandles) const
+{
+	check(ASC);
+
+	if (!ASC->IsOwnerActorAuthoritative())
+	{
+		return;
+	}
+
+	// Give Ability and Save Ability Handle
+	for (const FFPAbilitySystemData_Ability& AbilityData : AbilitiesToGrant)
+	{
+		if (AbilityData.AbilityClass && AbilityData.InputTag.IsValid())
+		{
+			FGameplayAbilitySpec AbilitySpec(AbilityData.AbilityClass, AbilityData.AbilityLevel);
+			AbilitySpec.DynamicAbilityTags.AddTag(AbilityData.InputTag);
+			const FGameplayAbilitySpecHandle SpecHandle = ASC->GiveAbility(AbilitySpec);
+			
+			if (OutGrantedHandles)
+			{
+				OutGrantedHandles->AddAbilitySpecHandle(SpecHandle);
+			}
+		}
+	}
+
+	// Apply Effect and Save Effect Handle
+	for (const FFPAbilitySystemData_Effect& EffectData : EffectsToGrant)
+	{
+		if (EffectData.EffectClass)
+		{
+			const UGameplayEffect* Effect = EffectData.EffectClass->GetDefaultObject<UGameplayEffect>();
+			FActiveGameplayEffectHandle EffectHandle = ASC->ApplyGameplayEffectToSelf(Effect, EffectData.EffectLevel, ASC->MakeEffectContext());
+			
+			if (OutGrantedHandles)
+			{
+				OutGrantedHandles->AddEffectHandle(EffectHandle);
+			}			
+		}
+	}
+}
