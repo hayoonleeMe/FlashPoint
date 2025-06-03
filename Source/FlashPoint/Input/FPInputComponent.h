f566f936925ffa444f88a5fa6b63beab0a162fbe
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-03 11:28:08 +0900

    UFPInputData에 저장된 모든 Ability InputAction에 콜백 함수를 등록하는 함수 구현

--- Source/FlashPoint/Input/FPInputComponent.h
+++ Source/FlashPoint/Input/FPInputComponent.h
@@ -18,6 +18,10 @@ public:
 	// InputTag에 해당하는 Native InputAction의 TriggerEvent에 Func를 등록한다.
 	template<class UserClass, typename FuncType>
 	void BindNativeAction(const UFPInputData* InputData, const FGameplayTag& InputTag, ETriggerEvent TriggerEvent, UserClass* Object, FuncType Func);
+
+	// InputData에 저장된 모든 Ability InputAction에 Triggered Event에 PressedFunc, Completed Event에 ReleasedFunc를 등록한다.
+	template<class UserClass, typename PressedFuncType, typename ReleasedFuncType>
+	void BindAbilityActions(const UFPInputData* InputData, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc);
 };
 
 template <class UserClass, typename FuncType>
@@ -28,5 +32,27 @@ void UFPInputComponent::BindNativeAction(const UFPInputData* InputData, const FG
 	if (const UInputAction* InputAction = InputData->FindNativeInputActionForInputTag(InputTag))
 	{
 		BindAction(InputAction, TriggerEvent, Object, Func);
+	}
+}
+
+template <class UserClass, typename PressedFuncType, typename ReleasedFuncType>
+void UFPInputComponent::BindAbilityActions(const UFPInputData* InputData, UserClass* Object, PressedFuncType PressedFunc, ReleasedFuncType ReleasedFunc)
+{
+	check(InputData);
+
+	for (const FInputConfig& InputConfig : InputData->AbilityInputConfigs)
+	{
+		if (InputConfig.InputAction && InputConfig.InputTag.IsValid())
+		{
+			if (PressedFunc)
+			{
+				BindAction(InputConfig.InputAction, ETriggerEvent::Triggered, Object, PressedFunc, InputConfig.InputTag);
+			}
+
+			if (ReleasedFunc)
+			{
+				BindAction(InputConfig.InputAction, ETriggerEvent::Completed, Object, ReleasedFunc, InputConfig.InputTag);
+			}
+		}
 	}
 }
