ba86fbc3b7f85c4e5ef6e30a9a0156c28c3c337c
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-06-07 21:53:47 +0900

    DA_InputData에 Input Asset 추가 - Input Tag 추가

---
+++ Source/FlashPoint/FPGameplayTags.cpp
@@ -0,0 +1,10 @@
+
+#include "FPGameplayTags.h"
+
+namespace FPGameplayTags
+{
+	UE_DEFINE_GAMEPLAY_TAG(Input_Action_Move, "Input.Action.Move");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Action_Look, "Input.Action.Look");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Action_Crouch, "Input.Action.Crouch");
+	UE_DEFINE_GAMEPLAY_TAG(Input_Action_Jump, "Input.Action.Jump");
+}
---
+++ Source/FlashPoint/FPGameplayTags.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace FPGameplayTags
+{
+	extern FNativeGameplayTag Input_Action_Move;
+	extern FNativeGameplayTag Input_Action_Look;
+	extern FNativeGameplayTag Input_Action_Crouch;
+	extern FNativeGameplayTag Input_Action_Jump;
+}
