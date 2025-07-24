d450564c159033f9d25e6036fc1b250fc3fbe50d
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-07-24 13:14:49 +0900

    캐릭터가 장착한 무기 액터의 Skeleton에 대한 Anim Montage를 재생하는 Anim Notify class 생성 잘못된 Anim Notify를 설정했던 Anim Montage들을 이 Anim Notify로 교체

---
+++ Source/FlashPoint/Animation/AnimNotify_PlayWeaponMontage.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_PlayWeaponMontage.generated.h"
+
+class AWeapon_Base;
+
+/**
+ * 캐릭터가 장착한 무기 액터의 Skeleton에 대한 Anim Montage를 재생한다.
+ */
+UCLASS()
+class FLASHPOINT_API UAnimNotify_PlayWeaponMontage : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+	// 캐릭터가 장착한 무기 액터를 반환한다.
+	static AWeapon_Base* GetEquippedWeapon(const USkeletalMeshComponent* MeshComp);
+
+protected:
+	UPROPERTY(EditAnywhere, Category=AnimNotify)
+	TObjectPtr<UAnimMontage> MontageToPlay;
+
+	UPROPERTY(EditAnywhere, Category=AnimNotify)
+	float RateScale = 1.f;
+};
