13900615c24690fcb01aa56ae35fc2504dd2e392
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-08-03 21:31:53 +0900

    Item Spawner (#42)
    * Spawner 액터의 SphereComponent에 사용할 Object Type Channel 추가
    
    * 무기, 탄약 등의 아이템을 소환하는 액터 AItemSpawner 구현
    - Item Spawner에 무기를 표시하기 위한 Static Mesh 추가 (각 무기의 Skeleton Mesh에서 Static Mesh 생성)
    - Item Spawner의 Pad Mesh로 사용할 Lyra 프로젝트의 애셋 추가

---
+++ Source/FlashPoint/Items/ItemSpawner.cpp
@@ -0,0 +1,135 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ItemSpawner.h"
+
+#include "FlashPoint.h"
+#include "FPLogChannels.h"
+#include "Components/SphereComponent.h"
+#include "Net/UnrealNetwork.h"
+#include "Weapon/WeaponManageComponent.h"
+#include "Weapon/Weapon_Base.h"
+
+AItemSpawner::AItemSpawner()
+{
+	bReplicates = true;
+	PrimaryActorTick.bCanEverTick = true;
+
+	PadMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Pad Mesh Component"));
+	SetRootComponent(PadMeshComponent);
+	PadMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Component"));
+	SphereComponent->SetupAttachment(PadMeshComponent);
+	SphereComponent->SetCollisionObjectType(ECC_Spawner);
+	SphereComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereComponent->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+
+	ItemMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Item Mesh Component"));
+	ItemMeshComponent->SetupAttachment(SphereComponent);
+	ItemMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+
+	MeshRotateSpeed = 20.f;
+}
+
+void AItemSpawner::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AItemSpawner, ActiveItemIndex);
+}
+
+void AItemSpawner::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	const FRotator CurrentRotator = SphereComponent->GetRelativeRotation();
+	SphereComponent->SetRelativeRotation(FRotator(CurrentRotator.Pitch, CurrentRotator.Yaw + MeshRotateSpeed * DeltaTime, CurrentRotator.Roll));
+}
+
+void AItemSpawner::OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex,
+										bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!OtherActor || !ItemInfos.IsValidIndex(ActiveItemIndex))
+	{
+		return;
+	}
+	
+	TSubclassOf<AActor> ItemClass = ItemInfos[ActiveItemIndex].ItemClass;
+	if (!ItemClass)
+	{
+		return;
+	}
+
+	if (ItemClass->IsChildOf<AWeapon_Base>())
+	{
+		if (UWeaponManageComponent* WeaponManageComp = OtherActor->FindComponentByClass<UWeaponManageComponent>())
+		{
+			TSubclassOf<AWeapon_Base> WeaponClass(ItemClass);
+			WeaponManageComp->EquipNewWeapon(WeaponClass);
+		}
+	}
+}
+
+void AItemSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (ItemInfos.IsEmpty())
+	{
+		UE_LOG(LogFP, Warning, TEXT("[%s] Item Classes is empty."), *GetName());
+		return;
+	}
+
+	if (HasAuthority())
+	{
+		SphereComponent->OnComponentBeginOverlap.AddDynamic(this, &ThisClass::OnSphereBeginOverlap);
+		GetWorldTimerManager().SetTimer(ItemChangeTimerHandle, FTimerDelegate::CreateUObject(this, &ThisClass::ChangeActiveItemIndex), ItemChangeDelay, true, 0.f);
+	}
+}
+
+#if WITH_EDITOR
+void AItemSpawner::PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent)
+{
+	Super::PostEditChangeProperty(PropertyChangedEvent);
+
+	if (PropertyChangedEvent.GetMemberPropertyName() == GET_MEMBER_NAME_CHECKED(AItemSpawner, ItemInfos))
+	{
+		// 첫번째 아이템을 미리보기로 표시한다.
+		// 아이템이 설정되지 않았으면 표시하지 않는다.
+		const int32 FirstItemIndex = ItemInfos.IsEmpty() ? -1 : 0; 
+		ShowItem(FirstItemIndex);
+	}
+}
+#endif
+
+void AItemSpawner::ChangeActiveItemIndex()
+{
+	// called on server only
+	const int32 NumItems = ItemInfos.Num();
+	ActiveItemIndex = FMath::RandRange(0, NumItems - 1);
+}
+
+void AItemSpawner::ShowItem(int32 ItemIndex)
+{
+	if (ItemIndex >= 0)
+	{
+		ItemMeshComponent->SetStaticMesh(ItemInfos[ItemIndex].ItemMesh);
+		ItemMeshComponent->SetRelativeTransform(ItemInfos[ItemIndex].ItemMeshTransform);
+		SphereComponent->SetSphereRadius(ItemInfos[ItemIndex].SphereRadius);
+		SphereComponent->SetRelativeLocation(FVector(0.f, 0.f, ItemInfos[ItemIndex].SphereRadius));
+	}
+	else
+	{
+		ItemMeshComponent->SetStaticMesh(nullptr);
+		ItemMeshComponent->SetRelativeTransform(FTransform::Identity);
+		SphereComponent->SetSphereRadius(0.f);
+		SphereComponent->SetRelativeLocation(FVector::ZeroVector);
+	}
+}
+
+void AItemSpawner::OnRep_ActiveItemIndex()
+{
+	// Show Item Mesh only in client
+	ShowItem(ActiveItemIndex);
+}
