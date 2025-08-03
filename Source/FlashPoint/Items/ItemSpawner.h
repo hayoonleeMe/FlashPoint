44264d08a90cfb4dd44309c30123c05be9c79cc6
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-08-03 21:31:04 +0900

    무기, 탄약 등의 아이템을 소환하는 액터 AItemSpawner 구현 - Item Spawner에 무기를 표시하기 위한 Static Mesh 추가 (각 무기의 Skeleton Mesh에서 Static Mesh 생성) - Item Spawner의 Pad Mesh로 사용할 Lyra 프로젝트의 애셋 추가

---
+++ Source/FlashPoint/Items/ItemSpawner.h
@@ -0,0 +1,89 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemSpawner.generated.h"
+
+class USphereComponent;
+
+USTRUCT(BlueprintType)
+struct FItemSpawnInfo
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AActor> ItemClass;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UStaticMesh> ItemMesh;
+
+	UPROPERTY(EditDefaultsOnly)
+	FTransform ItemMeshTransform;
+
+	UPROPERTY(EditDefaultsOnly)
+	float SphereRadius = 0.f;
+};
+
+/**
+ * 무기, 탄약 등의 아이템을 소환하는 액터
+ */
+UCLASS()
+class FLASHPOINT_API AItemSpawner : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AItemSpawner();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+	// Only called on server
+	UFUNCTION()
+	virtual void OnSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+	virtual void BeginPlay() override;
+
+#if WITH_EDITOR
+	virtual void PostEditChangeProperty(struct FPropertyChangedEvent& PropertyChangedEvent) override;
+#endif
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> PadMeshComponent;
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> SphereComponent;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> ItemMeshComponent;
+
+	// 소환할 아이템들
+	UPROPERTY(EditDefaultsOnly, Category="FlashPoint")
+	TArray<FItemSpawnInfo> ItemInfos;
+
+	// 아이템 회전 속도
+	UPROPERTY(EditDefaultsOnly, Category="FlashPoint")
+	float MeshRotateSpeed;
+
+	// 랜덤한 아이템을 선택할 시간
+	UPROPERTY(EditDefaultsOnly, Category="FlashPoint")
+	float ItemChangeDelay;
+
+	FTimerHandle ItemChangeTimerHandle;
+
+	// 서버에서 ActiveItemIndex를 랜덤하게 변경한다.
+	void ChangeActiveItemIndex();
+
+	// ItemIndex에 해당하는 아이템을 표시한다.
+	// ItemIndex가 유효하지 않으면 아이템을 숨긴다.
+	void ShowItem(int32 ItemIndex);
+
+	// 현재 선택된 아이템의 ItemInfos 배열 인덱스
+	UPROPERTY(ReplicatedUsing=OnRep_ActiveItemIndex)
+	int32 ActiveItemIndex;
+
+	UFUNCTION()
+	void OnRep_ActiveItemIndex();
+};
