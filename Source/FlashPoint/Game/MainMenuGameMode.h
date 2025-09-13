1c08d8d5ad8b61ee4592074142d2905240b0417f
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-09-13 17:33:15 +0900

    Main Menu Level 추가 - Level - PlayerController 블루프린트 - GameMode 블루프린트

---
+++ Source/FlashPoint/Game/MainMenuGameMode.h
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MainMenuGameMode.generated.h"
+
+UCLASS()
+class FLASHPOINT_API AMainMenuGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+};
