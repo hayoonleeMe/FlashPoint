35942c14bcf5b27a79d22438e51a5de7c93fba59
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-09-18 20:18:00 +0900

    팀 별로 플레이어를 소환하는 PlayerStart 클래스 구현 및 블루프린트 생성 - PadMeshComponent의 Static Mesh로 SM_launchpad_Round 설정 - Warehouse 레벨에 배치된 PlayerStart와 교체 -- Warehouse_TDM은 ATeamPlayerStart의 Team 설정

---
+++ Source/FlashPoint/Player/TeamPlayerStart.cpp
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TeamPlayerStart.h"
+
+#include "Components/CapsuleComponent.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(TeamPlayerStart)
+
+ATeamPlayerStart::ATeamPlayerStart(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+	Team = ETeam::None;
+
+	PadMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Pad Mesh Component"));
+	PadMeshComponent->SetupAttachment(GetRootComponent());
+	PadMeshComponent->SetRelativeLocation(FVector(0.f, 0.f, -GetCapsuleComponent()->GetScaledCapsuleHalfHeight()));
+	PadMeshComponent->SetRelativeScale3D(FVector(0.8f));
+}
+
+void ATeamPlayerStart::SetPadMeshColorByTeam()
+{
+	if (!TeamColorMID)
+	{
+		TeamColorMID = PadMeshComponent->CreateDynamicMaterialInstance(0);
+	}
+	
+	if (TeamColorMID)
+	{
+		switch (Team)
+		{
+		case ETeam::RedTeam:
+			TeamColorMID->SetVectorParameterValue(TEXT("Color"), FLinearColor::Red);
+			break;
+			
+		case ETeam::BlueTeam:
+			TeamColorMID->SetVectorParameterValue(TEXT("Color"), FLinearColor::Blue);
+			break;
+			
+		default: // FFA
+			TeamColorMID->SetVectorParameterValue(TEXT("Color"), FFAColor);
+			break;
+		}
+	}
+}
+
+#if WITH_EDITOR
+void ATeamPlayerStart::OnConstruction(const FTransform& Transform)
+{
+	SetPadMeshColorByTeam();
+}
+#endif
---
+++ Source/FlashPoint/Player/TeamPlayerStart.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Data/MatchTypes.h"
+#include "GameFramework/PlayerStart.h"
+#include "TeamPlayerStart.generated.h"
+
+/**
+ * 팀 별로 플레이어를 소환하는 PlayerStart
+ */
+UCLASS(PrioritizeCategories="Team")
+class FLASHPOINT_API ATeamPlayerStart : public APlayerStart
+{
+	GENERATED_BODY()
+
+public:
+	ATeamPlayerStart(const FObjectInitializer& ObjectInitializer);
+
+	ETeam GetTeam() const { return Team; }
+
+private:
+	UPROPERTY(EditAnywhere, Category="Team")
+	ETeam Team;
+
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UStaticMeshComponent> PadMeshComponent;
+
+	UPROPERTY()
+	TObjectPtr<UMaterialInstanceDynamic> TeamColorMID;
+
+	void SetPadMeshColorByTeam();
+
+	static constexpr FLinearColor FFAColor = FLinearColor(0.666667f, 0.587570f, 0.203389f);
+
+#if WITH_EDITOR
+public:
+	virtual void OnConstruction(const FTransform& Transform) override;
+#endif
+};
