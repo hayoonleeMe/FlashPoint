c0df7143839e5767392740da1eea566a6210e9af
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-09-18 21:49:45 +0900

    Warehouse Level에서 플레이어 팀 별로 랜덤한 PlayerStart에서 시작하도록 구현

--- Source/FlashPoint/Game/FPGameMode.cpp
+++ Source/FlashPoint/Game/FPGameMode.cpp
@@ -3,9 +3,12 @@
 
 #include "FPGameMode.h"
 
+#include "EngineUtils.h"
+#include "FPLogChannels.h"
 #include "Character/FPCharacter.h"
 #include "Player/FPPlayerController.h"
 #include "Player/FPPlayerState.h"
+#include "Player/TeamPlayerStart.h"
 
 #include UE_INLINE_GENERATED_CPP_BY_NAME(FPGameMode)
 
@@ -14,4 +17,46 @@ AFPGameMode::AFPGameMode()
 	DefaultPawnClass = AFPCharacter::StaticClass();
 	PlayerControllerClass = AFPPlayerController::StaticClass();
 	PlayerStateClass = AFPPlayerState::StaticClass();
+}
+
+AActor* AFPGameMode::ChoosePlayerStart_Implementation(AController* Player)
+{
+	if (Player)
+	{
+		if (TeamPlayerStartsMap.IsEmpty())
+		{
+			InitializeTeamPlayerStarts();
+		}
+		
+		if (ABasePlayerState* BasePS = Player->GetPlayerState<ABasePlayerState>())
+		{
+			// TDM과 FFA 모두 동일한 로직으로 처리 (FFA는 Team이 None)
+			ETeam PlayerTeam = BasePS->GetTeam();
+			if (const FTeamPlayerStartArray* TeamPlayerStartArray = TeamPlayerStartsMap.Find(PlayerTeam))
+			{
+				const TArray<TObjectPtr<ATeamPlayerStart>>& TeamPlayerStarts = TeamPlayerStartArray->TeamPlayerStarts;
+				if (!TeamPlayerStarts.IsEmpty())
+				{
+					const int32 RandIndex = FMath::RandRange(0, TeamPlayerStarts.Num() - 1);
+					return TeamPlayerStarts[RandIndex];
+				}
+			}
+		}
+	}
+	
+	UE_LOG(LogFP, Error, TEXT("[%hs] Can't find a proper player start. Falling back to AGameModeBase::ChoosePlayerStart()"), __FUNCTION__);
+	return Super::ChoosePlayerStart_Implementation(Player);
+}
+
+void AFPGameMode::InitializeTeamPlayerStarts()
+{
+	// 팀 별로 저장
+	for (TActorIterator<ATeamPlayerStart> It(GetWorld()); It; ++It)
+	{
+		if (It)
+		{
+			FTeamPlayerStartArray& TeamPlayerStartArray = TeamPlayerStartsMap.FindOrAdd(It->GetTeam());
+			TeamPlayerStartArray.TeamPlayerStarts.Add(*It);
+		}
+	}
 }
--- Source/FlashPoint/Game/FPGameMode.h
+++ Source/FlashPoint/Game/FPGameMode.h
@@ -6,6 +6,20 @@
 #include "BaseGameMode.h"
 #include "FPGameMode.generated.h"
 
+class ATeamPlayerStart;
+
+/**
+ * USTRUCT Wrapper for TArray
+ */
+USTRUCT()
+struct FTeamPlayerStartArray
+{
+	GENERATED_BODY()
+
+	UPROPERTY()
+	TArray<TObjectPtr<ATeamPlayerStart>> TeamPlayerStarts;
+};
+
 /**
  * 게임플레이에서 사용할 GameModeBase
  */
@@ -16,4 +30,13 @@ class FLASHPOINT_API AFPGameMode : public ABaseGameMode
 
 public:
 	AFPGameMode();
+	virtual AActor* ChoosePlayerStart_Implementation(AController* Player) override;
+
+private:
+	// 레벨에 존재하는 모든 ATeamPlayerStart 액터를 캐싱한다.
+	void InitializeTeamPlayerStarts();
+
+	// 팀 별로 ATeamPlayerStart 액터 배열을 저장하는 맵
+	UPROPERTY()
+	TMap<ETeam, FTeamPlayerStartArray> TeamPlayerStartsMap;
 };
