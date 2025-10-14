246c9d87c732fc63244bb2ac91ce85b6dc04f49e
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-10-14 18:08:35 +0900

    Notify를 받을 때 Owner에 Gameplay Event를 전송하는 Anim Notify 클래스 생성 각 무기의 Reload Anim Montage에 이 Anim Notify 추가

---
+++ Source/FlashPoint/Animation/AnimNotify_SendGameplayEvent.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimNotify_SendGameplayEvent.h"
+
+#include "AbilitySystemBlueprintLibrary.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(AnimNotify_SendGameplayEvent)
+
+void UAnimNotify_SendGameplayEvent::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	Super::Notify(MeshComp, Animation, EventReference);
+
+	if (EventTag.IsValid() && MeshComp)
+	{
+		if (AActor* Owner = MeshComp->GetOwner())
+		{
+			const UWorld* World = Owner->GetWorld();
+			// 이 Notify를 추가한 Anim Montage 애셋을 열 때 ASC를 가지고 있지 않은 Animation Editor Preview Actor에 의해 Error Log가 출력되는 것을 방지하기 위해 GameWorld인지 체크
+			if (World && World->IsGameWorld())
+			{
+				UAbilitySystemBlueprintLibrary::SendGameplayEventToActor(Owner, EventTag, FGameplayEventData());
+			}
+		}
+	}
+}
+
---
+++ Source/FlashPoint/Animation/AnimNotify_SendGameplayEvent.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AnimNotify_SendGameplayEvent.generated.h"
+
+/**
+ * Notify를 받을 때 Owner에 Gameplay Event를 전송하는 Anim Notify 
+ */
+UCLASS()
+class FLASHPOINT_API UAnimNotify_SendGameplayEvent : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+	UPROPERTY(EditAnywhere, Category=AnimNotify)
+	FGameplayTag EventTag;
+};
