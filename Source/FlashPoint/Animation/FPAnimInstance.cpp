411260586f06924b88df0dd9edebed1a36f55d8f
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2025-10-17 15:04:15 +0900

    Turn In Place 구현 - UFPAnimInstance에서 NativeUpdateAnimation() 대신 NativeThreadSafeUpdateAnimation()를 사용하도록 변경 - RootYawOffset 변수를 추가하고 bUseControllerRotationYaw 옵션을 사용하는 캐릭터가 Idle State일 때 시점을 변경해도 회전하지 않도록 Anim Graph에 추가한 Rotate Root Bone 노드의 Yaw로 사용 - ABP_Player의 Idle State의 Output Animation Pose의 On Update에 연동한 Update Idle State 함수에서, ProcessTurnYawCurve() 함수를 호출해 Turn Animation에 설정된 Curve 값을 이용해 Root Yaw Offset 값을 감소(절대값 기준)시켜 Turn Animation이 재생되는 동안 실제 캐릭터를 회전시킴 (Rotate Root Bone 노드에 의해) -- AimYaw를 -RootYawOffset으로 설정해 회전한 시점 방향으로 Aim Offset 재생 - ABP_WeaponLayers의 FullBody_Idle Layer에 State Machine을 추가하고 Idle State, TurnInPlace State, TurnInPlaceRecovery State 추가 -- RootYawOffset 절대값이 특정 값을 넘어가면 TurnInPlace State로 전환되고 Turn Animation을 Sequence Evaluator로 재생하고 Delta Time마다 늘어나는 Turn Anim Time을 통해 Evaluator의 Explicit Time 설정 -- Turn Animation의 Turn Yaw Weight Curve Value가 0이 되면 TurnInPlaceRecovery State로 전환되고 Turn Anim Time을 Start Position으로 하는 Sequence Player를 이용해 남은 애니메이션을 재생하고 끝나면 Idle State로 전환 -- TurnInPlaceRecovery State에서, Idle -> TurnInPlace로 전환되는 것과 동일한 상황이라면 다시 TurnInPlace로 전환해 다시 Turn Animation 재생 (계속해서 시점을 변경해 시점을 따라 쭉 Turn Animation 재생하며 회전하는 경우) - Weapon Layer 별 Turn Animation 설정

--- Source/FlashPoint/Animation/FPAnimInstance.cpp
+++ Source/FlashPoint/Animation/FPAnimInstance.cpp
@@ -21,6 +21,10 @@ const FVector2D UFPAnimInstance::LandRecoveryAlphaOutRange = { 0.f, 1.f };
 UFPAnimInstance::UFPAnimInstance()
 {
 	CardinalDirectionDeadZone = 10.f;
+	RootYawOffsetAngleClamp = { -120.f, 100.f };
+	RootYawOffsetAngleCrouchClamp = { -90.f, 80.f };
+	TurnYawWeightCurveName = TEXT("TurnYawWeight");
+	RemainingTurnYawCurveName = TEXT("RemainingTurnYaw");
 }
 
 void UFPAnimInstance::NativeInitializeAnimation()
@@ -65,17 +69,25 @@ void UFPAnimInstance::GameplayTagEventCallback(const FGameplayTag Tag, int32 New
 	}
 }
 
-void UFPAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+void UFPAnimInstance::NativeThreadSafeUpdateAnimation(float DeltaSeconds)
 {
-	Super::NativeUpdateAnimation(DeltaSeconds);
+	Super::NativeThreadSafeUpdateAnimation(DeltaSeconds);
 
-	if (ACharacter* Character = Cast<ACharacter>(GetOwningActor()))
+	if (const ACharacter* Character = Cast<ACharacter>(GetOwningActor()))
 	{
 		if (const UCharacterMovementComponent* MoveComponent = Character->GetCharacterMovement())
 		{
+			const FRotator Rotation = Character->GetActorRotation();
+			if (!bIsFirstUpdate)
+			{
+				// 첫 업데이트라면 초기값인 WorldRotation과 캐릭터 Rotation의 간극이 크므로 두번째 업데이트부터 계산
+				YawDeltaSinceLastUpdate = Rotation.Yaw - WorldRotation.Yaw; 
+			}
+			WorldRotation = Rotation;
+			
 			Velocity = MoveComponent->Velocity;
 			GroundSpeed = Velocity.Size2D();
-			DirectionAngle = UKismetAnimationLibrary::CalculateDirection(Velocity, Character->GetActorRotation());
+			DirectionAngle = UKismetAnimationLibrary::CalculateDirection(Velocity, WorldRotation);
 			
 			bShouldMove = GroundSpeed > 0.f && MoveComponent->GetCurrentAcceleration() != FVector::ZeroVector;
 			bIsOnGround = MoveComponent->IsMovingOnGround();
@@ -85,6 +97,12 @@ void UFPAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
 			UpdateAimingData(Character);
 			UpdateCurrentDirection();
 			UpdateBlendWeight(DeltaSeconds);
+			UpdateRootYawOffset(DeltaSeconds);
+
+			if (bIsFirstUpdate)
+			{
+				bIsFirstUpdate = false;
+			}
 		}
 	}
 }
@@ -218,5 +236,68 @@ void UFPAnimInstance::UpdateLeftHandModifyTransform()
 	else
 	{
 		LeftHandModifyAlpha = 0.f;
+	}
+}
+
+void UFPAnimInstance::SetRootYawOffset(float InValue)
+{
+	RootYawOffset = UKismetMathLibrary::NormalizeAxis(InValue);
+
+	// RootYawOffset이 과도하게 커지는(절대값 기준) 경우를 방지하기 위해 Clamp
+	if (bIsCrouching)
+	{
+		RootYawOffset = FMath::ClampAngle(RootYawOffset, RootYawOffsetAngleCrouchClamp.X, RootYawOffsetAngleCrouchClamp.Y);
+	}
+	else
+	{
+		RootYawOffset = FMath::ClampAngle(RootYawOffset, RootYawOffsetAngleClamp.X, RootYawOffsetAngleClamp.Y);
+	}
+
+	// 실제 시점 방향으로 Aim Offset 재생
+	AimYaw = -RootYawOffset;
+}
+
+void UFPAnimInstance::UpdateRootYawOffset(float DeltaSeconds)
+{
+	if (!bIsTurnInPlaceBlendingOut)
+	{
+		// Idle State인 경우, 시점 변경한 만큼 Offset 증가 (시점 방향과 반대로 증가시키기 위해 Delta를 빼줌)
+		SetRootYawOffset(RootYawOffset - YawDeltaSinceLastUpdate);
+	}
+	else
+	{
+		// Idle State가 아닌 경우(Idle에서 빠져나오는 Blending Out 포함), 0으로 Interpolate해 부드럽게 캐릭터의 Root가 시점 방향과 일치하도록 회전
+		SetRootYawOffset(FMath::FInterpTo(RootYawOffset, 0.f, DeltaSeconds, 10.f));
+	}
+
+	// Idle State에서 false로 설정되는 경우를 제외하곤 항상 Blending Out
+	bIsTurnInPlaceBlendingOut = true;
+}
+
+void UFPAnimInstance::ProcessTurnYawCurve()
+{
+	// Idle State에서 호출되므로 false로 설정
+	bIsTurnInPlaceBlendingOut = false;
+
+	// 이전 프레임 값으로 업데이트
+	PreviousTurnYawCurveValue = TurnYawCurveValue;
+
+	// Turn Animation의 Curve 값을 이용해 RootYawOffset을 감소(절대값 기준)시켜 시점 방향으로 캐릭터의 Root Bone을 회전시킴
+	const float TurnYawWeight = GetCurveValue(TurnYawWeightCurveName);
+	if (!FMath::IsNearlyZero(TurnYawWeight))
+	{
+		const float RemainingTurnYaw = GetCurveValue(RemainingTurnYawCurveName);
+		TurnYawCurveValue = RemainingTurnYaw / TurnYawWeight;
+
+		if (PreviousTurnYawCurveValue != 0.f)
+		{
+			const float TurnYawCurveDelta = TurnYawCurveValue - PreviousTurnYawCurveValue;
+			SetRootYawOffset(RootYawOffset - TurnYawCurveDelta);
+		}
+	}
+	else
+	{
+		TurnYawCurveValue = 0.f;
+		PreviousTurnYawCurveValue = 0.f;
 	}
 }
