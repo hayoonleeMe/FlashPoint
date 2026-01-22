fdf4f7079eb8b1f41499f07985f56321ab76b538
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2026-01-23 01:46:06 +0900

    Sprint Ability 개선 - UFPCharacterMovementComponent 기능을 사용하도록 변경 - IsMovingForwardFromInput 태그를 체크해 전방으로 이동할 때만 Sprint 가능하도록 변경

--- Source/FlashPoint/AbilitySystem/Abilities/FPGameplayAbility_Sprint.h
+++ Source/FlashPoint/AbilitySystem/Abilities/FPGameplayAbility_Sprint.h
@@ -23,12 +23,4 @@ protected:
 	virtual bool CanActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayTagContainer* SourceTags = nullptr, const FGameplayTagContainer* TargetTags = nullptr, FGameplayTagContainer* OptionalRelevantTags = nullptr) const override;
 	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
 	virtual void EndAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, bool bReplicateEndAbility, bool bWasCancelled) override;
-
-private:
-	// 이동속도 증가 정도 (이동속도에 곱해짐)
-	UPROPERTY(EditDefaultsOnly)
-	float SpeedMultiplier;
-
-	// 증가 전 기존 이동속도
-	float BaseWalkSpeed = 0.f;
 };
