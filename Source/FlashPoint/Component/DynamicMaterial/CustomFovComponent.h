ab4807d818a96000aaf4cf36bebe6afb853d2b25
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2026-03-06 23:08:19 +0900

    CustomFOV 기능 구현 - 메시에 카메라와 별개인 CustomFOV를 적용하는 기능 - MF_CustomFOV를 World Position Offset에 연결해 사용
    CustomFOV 적용을 위해 머터리얼 수정
    - MF_CustomFOV를 World Position Offset에 연결
    - 캐릭터 메시 머터리얼
    - 무기 마스터 머터리얼
    - 부착물 마스터 머터리얼
    
    등록한 메시에 카메라와 별개인 Custom FOV를 적용하는 컴포넌트 클래스 UCustomFovComponent 추가
    - MF_CustomFOV에서 사용하는 파라미터를 관리한다.
    - UDynamicMaterialComponent 하위 클래스

---
+++ Source/FlashPoint/Component/DynamicMaterial/CustomFovComponent.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DynamicMaterialComponent.h"
+#include "CustomFovComponent.generated.h"
+
+/**
+ * 등록한 메시에 카메라와 별개인 Custom FOV를 적용하는 컴포넌트
+ * MF_CustomFOV에서 사용하는 파라미터를 관리한다.
+ * 
+ */
+UCLASS()
+class FLASHPOINT_API UCustomFovComponent : public UDynamicMaterialComponent
+{
+	GENERATED_BODY()
+	
+public:
+	UCustomFovComponent();
+	
+	// 새로운 컴포넌트를 생성하고 등록한 뒤 반환한다.
+	static UCustomFovComponent* CreateComponent(
+		UObject* Outer,
+		const FName& Name,
+		UAbilitySystemComponent* ASC,
+		const TArray<UMeshComponent*>& MeshesToRegister
+	);
+	
+	// CustomFOV 파라미터 값을 설정한다.
+	void SetCustomFOV(float Value);
+	
+	// EnableCustomFOV 파라미터 값을 설정한다.
+	void SetEnableCustomFOV(float Value);
+	
+	// ScaleInDepth 파라미터 값을 설정한다.
+	void SetScaleInDepth(float Value);
+	
+	// ScreenOffset 파라미터 값을 설정한다.
+	void SetScreenOffset(const FVector& Value);
+};
