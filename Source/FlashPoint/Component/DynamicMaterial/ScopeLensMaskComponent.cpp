bab33a1eefe2c6117193330f634645343072e31d
hayoonlee <73100820+hayoonleeMe@users.noreply.github.com>
2026-03-07 01:42:43 +0900

    ScopeLensMask 기능 구현 - 현재 메시에 Relative한 Scope Lens 위치를 기준으로 중심을 보는 방향으로 Lens Radius만큼  투명하게 만드는 기능 - MF_ScopeLensMask를 Opacity Mask에 연결해 적용
    ScopeLensMask 적용을 위해 머터리얼 수정
    - MF_ScopeLensMask를 Opacity Mask에 적용
    - 스코프 메시 머터리얼
    
    등록한 메시에 스코프 렌즈를 기준으로 일정 Radius만큼 투명 마스크를 적용하는 컴포넌트 클래스 UScopeLensMaskComponent 추가
    - MF_ScopeLensMask에서 사용하는 파라미터를 관리한다.
    - UDynamicMaterialComponent 하위 클래스

---
+++ Source/FlashPoint/Component/DynamicMaterial/ScopeLensMaskComponent.cpp
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ScopeLensMaskComponent.h"
+
+#include "FPGameplayTags.h"
+
+#include UE_INLINE_GENERATED_CPP_BY_NAME(ScopeLensMaskComponent)
+
+namespace
+{
+	const FName EnableScopeLensMaskName = TEXT("EnableScopeLensMask");
+	const FName LensRadiusName = TEXT("LensRadius");
+	const FName LocalLensLocationName = TEXT("LocalLensLocation");
+}
+
+UScopeLensMaskComponent::UScopeLensMaskComponent()
+{
+	ScalarMatParamConfigs.Add({ EnableScopeLensMaskName, true });
+	ScalarMatParamConfigs.Add({ LensRadiusName, true });
+	VectorMatParamConfigs.Add({ LocalLensLocationName, true });
+	DefaultRequiredTags.AddTag(FPGameplayTags::CharacterState::IsAimingDownSight);
+	MaterialActivationConfigs.Add({ EnableScopeLensMaskName, 1.f, 0.f });
+}
+
+UScopeLensMaskComponent* UScopeLensMaskComponent::CreateComponent(UObject* Outer, const FName& Name, UAbilitySystemComponent* ASC,
+	const TArray<UMeshComponent*>& MeshesToRegister)
+{
+	return Cast<UScopeLensMaskComponent>(CreateComponent_Internal(Outer, StaticClass(), Name, ASC, MeshesToRegister));
+}
+
+void UScopeLensMaskComponent::SetEnableScopeLensMask(float Value)
+{
+	SetScalarParameterValue(EnableScopeLensMaskName, Value);
+}
+
+void UScopeLensMaskComponent::SetLensRadius(float Value)
+{
+	SetScalarParameterValue(LensRadiusName, Value);
+}
+
+void UScopeLensMaskComponent::SetLocalLensLocation(const FVector& Value)
+{
+	SetVectorParameterValue(LocalLensLocationName, Value);
+}
---
+++ Source/FlashPoint/Component/DynamicMaterial/ScopeLensMaskComponent.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DynamicMaterialComponent.h"
+#include "ScopeLensMaskComponent.generated.h"
+
+/**
+ * 등록한 메시에 스코프 렌즈를 기준으로 일정 Radius만큼 투명 마스크를 적용하는 컴포넌트
+ * MF_ScopeLensMask에서 사용하는 파라미터를 관리한다.
+ */
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FLASHPOINT_API UScopeLensMaskComponent : public UDynamicMaterialComponent
+{
+	GENERATED_BODY()
+
+public:
+	UScopeLensMaskComponent();
+	
+	// 새로운 컴포넌트를 생성하고 등록한 뒤 반환한다.
+	static UScopeLensMaskComponent* CreateComponent(
+		UObject* Outer,
+		const FName& Name,
+		UAbilitySystemComponent* ASC,
+		const TArray<UMeshComponent*>& MeshesToRegister
+	);
+	
+	// EnableScopeLensMask 파라미터 값을 설정한다.
+	UFUNCTION(BlueprintCallable)
+	void SetEnableScopeLensMask(float Value);
+	
+	// LensRadius 파라미터 값을 설정한다.
+	UFUNCTION(BlueprintCallable)
+	void SetLensRadius(float Value);
+	
+	// LocalLensLocation 파라미터 값을 설정한다.
+	UFUNCTION(BlueprintCallable)
+	void SetLocalLensLocation(const FVector& Value);
+};
